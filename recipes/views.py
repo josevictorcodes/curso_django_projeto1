bc7baa643ad3a12c3f167d6ea4e50009b0d212a1
Jose Victor Gomes <jvprogramcoding@gmail.com>
2025-05-12 23:54:24 -0300

    importand font-awesome icons

--- recipes/views.py
+++ recipes/views.py
@@ -8,9 +8,3 @@ from django.http import HttpResponse
 def home(request):
     """Receives a request and return a html file inside the templates folder"""
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
