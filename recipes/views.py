0b0e7b70527e5b2b5ea8096d710c456eb631d1de
Jose Victor Gomes <jvprogramcoding@gmail.com>
2025-05-13 23:27:22 -0300

    head.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 #from django.urls import path
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 def home(request):
     """Receives a request and return a html file inside the templates folder"""
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
